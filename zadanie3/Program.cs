3a010a2e31bfcd437cf4973821427baf53e7aed4
Aleksandr Kamaev <al.kamaev@yandex.ru>
2022-07-25 22:44:25 +0300

    Добавили программу,проверяющую,является ли число палиндромом

---
+++ zadanie3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программa, которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом.
+Console.Write("Число N=");
+int N=Convert.ToInt32(Console.ReadLine());
+int temp=N;
+int rev=0;
+while (N>0)
+{
+   int dig=N%10;    
+   rev=rev*10+dig;
+   N=N/10;
+}
+
+if(temp==rev)
+{
+Console.Write("Число является палиндромом");
+Console.WriteLine();
+}
+else
+{
+Console.Write("Число не палиндром"); 
+Console.WriteLine();  
+}
+
+
+
+
+
+
